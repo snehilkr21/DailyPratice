6e39566ee1b9ce72f3b150575c3642a0884fac38
snehil <snehil@snehils-MacBook-Air.local>
2023-12-11 10:11:56 +0530

    Block and Block scope

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 
-function b(){
-    c()
-    let x=10;
-    function c(){
-          console.log(x,a)
-    }
-   
-}
 var a=10;
-b()
+let b=20;
+const c=30;
+function d(){
+    var a1=10;
+    let b1=20;
+    const c1=30;
+    {
+        var a11=10;
+        let b11=20;
+        const c11=30;
+        {
+            var a111=10;
+            let b111=20;
+            const c111=30;
+        }
+    }
+}
+d()
