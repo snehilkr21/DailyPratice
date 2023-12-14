0ff37de32a9657e87057c1fb34f1f717b65a5e61
snehil <snehil@snehils-MacBook-Air.local>
2023-12-14 19:29:27 +0530

    closures

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-
-var a=10;
-let b=20;
-const c=30;
-function d(){
+function a(){
     var a1=10;
-    let b1=20;
-    const c1=30;
-    {
-        var a11=10;
-        let b11=20;
-        const c11=30;
-        {
-            var a111=10;
-            let b111=20;
-            const c111=30;
+    function b(){
+        var b1=20;
+        function c(){
+            console.log(a1,b1)
         }
+        return c;
     }
+    return b()
 }
-d()
+var d=a()
+console.log(d)
+console.log(d())
