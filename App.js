1dfbc0c56d841100a6c61a33c96a4c27294070a0
snehil <snehil@192.168.1.5>
2023-12-27 01:29:46 +0530

    promise

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-const cart=["shoes","pants","kurta"]
-
-api.createOrder(cart,function (){
-  api.proceedToPayment(function (){
-    api.showOrderSummary(function (){
-       api.updateWallet()
-    })
-  })
+const GITHUB_API ="https://api.github.com/users/snehilkr21"
+const user =fetch(GITHUB_API);
+user.then((data)=>{
+  console.log("data",data)
 })
+for(let i=0;i<5;i++)
+  console.log("i love js")
