381aae33526a3e84d9e6c612591d31401b18e09f
snehil <snehil@192.168.1.9>
2023-12-29 01:57:11 +0530

    polyfill for race

--- App.js
+++ App.js
@@ -1,40 +1,27 @@
-// Check if Promise.allSettled already exists, if not, create a polyfill
-function mySettled (promises) {
-  const results = [];
-      let completed = 0;
-
-      if (promises.length === 0) {
-        resolve(results);
-      }
-
-      const checkCompletion = () => {
-        if (completed === promises.length) {
-          resolve(results);
-        }
-      };
-
-      promises.forEach((promise, index) => {
-        Promise.resolve(promise)
-          .then((value) => {
-            results[index] = { status: 'fulfilled', value };
-          })
-          .catch((reason) => {
-            results[index] = { status: 'rejected', reason };
-          })
-      })
-    
+function myRace(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          resolve(value);
+        })
+        .catch((reason) => {
+          reject(reason);
+        });
+    });
+  });
 }
 
 // Example usage
-const promise1 = Promise.resolve(3);
-const promise2 = new Promise((_, reject) => setTimeout(reject, 100, 'Error'));
-const promise3 = new Promise((resolve) => setTimeout(resolve, 200, 'Resolved'));
+const promise1 = new Promise((resolve) => setTimeout(resolve, 500, 'First'));
+const promise2 = new Promise((resolve) => setTimeout(resolve, 300, 'Second'));
+const promise3 = new Promise((_, reject) => setTimeout(reject, 200, 'Third'));
 
 const allPromises = [promise1, promise2, promise3];
 
-Promise.allSettled(allPromises)
-  .then((results) => {
-    console.log("result",results);
+myRace(allPromises)
+  .then((result) => {
+    console.log('First settled:', result);
   })
   .catch((error) => {
     console.error('Error occurred:', error);
