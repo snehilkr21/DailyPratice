6d285ac2ea51b451135b4640dd2475a60d1f9d0a
snehil <snehil@192.168.1.9>
2023-12-28 17:48:54 +0530

    creating a promise

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-const GITHUB_API ="https://api.github.com/users/snehilkr21"
-const user =fetch(GITHUB_API);
-user.then((data)=>{
-  console.log("data",data)
+const cart = ["kurta", "pants", "shirts"];
+
+const promise = createOrder(cart);
+promise
+  .then(function (orderId) {
+    console.log("orderId", orderId);
 })
-for(let i=0;i<5;i++)
-  console.log("i love js")
+  .catch(function (err) {
+    console.log(err.message);
+});
+console.log("promise", promise);
+
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    const accept = true; // Set to true for demonstration purposes
+    if (!accept) {
+      const err = new Error("Not valid");
+      reject(err);
+    } else {
+      resolve("12345");
+    }
+  });
+  return pr;
+}
