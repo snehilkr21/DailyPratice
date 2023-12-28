407d4c0adcb019df6f2a19c3d11b3c23a238f554
snehil <snehil@192.168.1.9>
2023-12-28 23:39:39 +0530

    Promise.all()

--- App.js
+++ App.js
@@ -1,41 +1,58 @@
-const cart = ["kurta", "pants", "shirts"];
+const prom1 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve("gfg1")
+    }, 1000)
+})
 
-createOrder(cart)
-.then(function (orderId) {
-    console.log("orderId", orderId);
-    return orderId;
+const prom2 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        reject("error")
+    }, 4000)
 })
-.catch(function(err){
-    console.log(err.message)
-})
-.then(function(orderId){
-    return proccedToPayment(orderId)
-})
-.then(function(paymentInfo){
-    console.log("paymentInfo",paymentInfo)
-})
-.catch(function (err) {
-    console.log(err.message);
-})
-.then(function(){
-  console.log("I will be called")
-})
-console.log("promise", promise);
 
-function createOrder(cart) {
-  const pr = new Promise(function (resolve, reject) {
-    const accept = false; // Set to true for demonstration purposes
-    if (!accept) {
-      const err = new Error("Not valid");
-      reject(err);
-    } else {
-      resolve("12345");
-    }
-  });
-  return pr;
-}
-function proccedToPayment(orderId){
-    return new Promise(function (resolve,reject){
-        resolve(`åpayment done successfully,${orderId}`)
+const prom3 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve("gfg2")
+    }, 3000)
+})
+
+const prom4 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve("gfg3")
+    }, 3000)
+})
+
+Promise.myall = function (values) {
+    const promise = new Promise(function (resolve, reject) {
+        let result = [];
+        let total = 0;
+        values.forEach((item, index) => {
+            console.log(`xcv${index}`, item);
+            Promise.resolve(item)
+                .then((res) => {
+                    console.log('dfg1', res);
+                    result[index] = res;
+                    total++;
+                    if (total === values.length)
+                        resolve(result);
+                })
+                .catch((err) => {
+                    console.log('dfg2', err);
+                    reject(err);
+                })
+        })
     })
+    return promise
 }
+
+Promise.myall([
+    prom1,
+    prom3,
+    prom4
+])
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((er) => {
+        console.log(er)
+    })
