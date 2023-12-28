427e032c223902e2f6a7c06cd39fec0ec3f3f28f
snehil <snehil@192.168.1.9>
2023-12-29 02:21:01 +0530

    .any pollyfill

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-function myRace(promises) {
+function myAny(promises) {
   return new Promise((resolve, reject) => {
+    if (promises.length === 0) {
+      reject(new AggregateError('No promises were provided'));
+    }
+
+    const errors = [];
+    let completed = 0;
+
+    const checkCompletion = () => {
+      if (completed === promises.length) {
+        reject(new AggregateError('All promises were rejected', errors));
+      }
+    };
+
     promises.forEach((promise) => {
       Promise.resolve(promise)
         .then((value) => {
           resolve(value);
         })
         .catch((reason) => {
-          reject(reason);
+          errors.push(reason);
+          completed++;
+          checkCompletion();
         });
     });
   });
@@ -19,9 +34,9 @@ const promise3 = new Promise((_, reject) => setTimeout(reject, 200, 'Third'));
 
 const allPromises = [promise1, promise2, promise3];
 
-myRace(allPromises)
+myAny(allPromises)
   .then((result) => {
-    console.log('First settled:', result);
+    console.log('Resolved:', result);
   })
   .catch((error) => {
     console.error('Error occurred:', error);
