30435bd1c841d5e8d4adcf061d60f1a98a25cb98
snehil <snehil@192.168.1.7>
2024-01-02 15:20:49 +0530

    async function return a promise

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+const p = new Promise((resolve,reject)=>{
+   resolve("Promise Ressolved")
+})
+
 async function getData(){
-  
+  return p;
 }
+const dataPromise = getData()
+dataPromise.then((res)=>console.log("res",res))
+console.log(dataPromise)
