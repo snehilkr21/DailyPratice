21d4958e8e843bda2cf5cc8579ebef2239b6f211
snehil <snehil@192.168.1.7>
2024-01-02 14:01:56 +0530

    to create async function

--- App.js
+++ App.js
@@ -1,17 +1,3 @@
-const p = new Promise((resolve,reject)=>{
-    resolve("Done")
-})
-
-//handle Promise earlier
-function getData(){
-  p
-  .then((res)=> console.log("res",res))
+async function getData(){
+  
 }
-getData()
-
-//handle Promise using async and await 
-async function handlePromise(){
-  const value=await p
-  console.log("value",value)
-}
-handlePromise()
