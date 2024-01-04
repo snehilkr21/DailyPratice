690b99ab868dbb6c2880b7a748a301f6027244bd
snehil <snehil@192.168.1.4>
2024-01-05 01:08:39 +0530

    handling error in async and await

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 
 async function handlePromise(){
-     let data = await fetch(`https://api.github.com/users/snehilkr21`)
-     //it gives a promise which contains the readable stream , if want to convert that readable stream to json then 
-     //use .json , using .json also gives a promise that's why we use wait keyword and it need time to resolve
-     //and when this promise is resolved it gives the actual result as we want.
+  try{
+    let data = await fetch(`https://apii.github.com/users/snehilkr21`)
      data = await data.json()
      console.log("data",data)
+  }
+  catch(err){
+    console.log("err",err)
+  }
+
 }
 handlePromise()
 
