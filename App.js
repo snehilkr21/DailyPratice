b4a7013c054fc902440f1a8450c8937074b3e3bd
snehil <snehil@192.168.1.4>
2024-01-07 14:56:51 +0530

    value of this inside global is the window object

--- App.js
+++ App.js
@@ -1,15 +1,1 @@
-
-async function handlePromise(){
-  try{
-    let data = await fetch(`https://apii.github.com/users/snehilkr21`)
-     data = await data.json()
-     console.log("data",data)
-  }
-  catch(err){
-    console.log("err",err)
-  }
-
-}
-handlePromise()
-
-
+console.log(this)
