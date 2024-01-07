ec88dd5b40037f872fd07298dd55734cbc0a5e59
snehil <snehil@192.168.1.4>
2024-01-07 18:15:10 +0530

    passing the value of this keyword to a method

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-// "use strict"
-const obj = {
-  a : 10,
-  x : function(){
-    console.log(this)
+"use strict"
+const student1 = {
+  name : "Snehil",
+  printName : function(){
+    console.log(this.name)
   }
 }
-obj.x()
+student1.printName()
+
+const student2 = {
+  name : "Shashank raj",
+}
+student1.printName.call(student2)
+student1.printName()
