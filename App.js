7e659a324c416f524c4f8840bf9785820dbd0aab
snehil <snehil@192.168.1.4>
2024-01-07 18:34:47 +0530

    value of this inside arrow fuction points to its lexical env

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 "use strict"
 const student1 = {
   name : "Snehil",
-  printName : function(){
-    console.log(this.name)
+  printName : () =>{
+    console.log(this)
   }
 }
-student1.printName()
-
-const student2 = {
-  name : "Shashank raj",
-}
-student1.printName.call(student2)
 student1.printName()
