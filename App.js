5626f1288ccbae220aeb178cc4ce0af031706b41
snehil <snehil@192.168.1.4>
2024-01-07 18:51:31 +0530

    value of this keyword inside nested arrow function

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 "use strict"
 const student1 = {
   name : "Snehil",
-  printName : () =>{
-    console.log(this)
+  printName : () => {
+    const x = () =>{
+      console.log(this)
+    }
+    x()
   }
 }
 student1.printName()
