f89da9cf8225233ef2285f445777ee0957eb1e5a
snehil <snehil@192.168.1.8>
2024-01-17 22:05:44 +0530

    combination of true and false as a third parameter

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
 document.getElementById("grandParent").addEventListener("click",()=>{console.log("grandparent is cliked")},true)
-document.getElementById("parent").addEventListener("click",()=>{console.log("parent is cliked")},true)
+document.getElementById("parent").addEventListener("click",()=>{console.log("parent is cliked")},false)
 document.getElementById("child").addEventListener("click",()=>{console.log("child is cliked")},true)
