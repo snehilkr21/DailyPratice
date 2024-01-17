2340c7b29f55602b6864a74255d6cc1d5e7a93a7
snehil <snehil@192.168.1.8>
2024-01-17 22:49:19 +0530

    stopPropagation

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
 document.getElementById("grandParent").addEventListener("click",()=>{console.log("grandparent is cliked")},true)
-document.getElementById("parent").addEventListener("click",()=>{console.log("parent is cliked")},false)
-document.getElementById("child").addEventListener("click",()=>{console.log("child is cliked")},true)
+document.getElementById("parent").addEventListener("click",(e)=>{
+  console.log("parent is cliked") 
+  e.stopPropagation()
+  }  
+  ,true)
+document.getElementById("child").addEventListener("click",(e)=>{
+  console.log("child is cliked")
+},true)
