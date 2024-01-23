2de55e1c4de2e3fd887e7f63f44effa34159d861
snehil <snehil@192.168.1.4>
2024-01-23 23:13:13 +0530

    bind method

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-document.getElementById("category").addEventListener("click",
-(e)=>{
-  e.stopPropagation()
-  if(e.target.tagName =="LI")
-  window.location.href = "/"+e.target.id
-  console.log("LI is clicked")
-})
+let name1 = {
+  firstName : "Snehil",
+  lastName :  "kumar",
+}
 
-document.getElementById("div").addEventListener("click",
-(e)=>{
-  console.log("div is clicked")
-})
+let name2 = {
+  firstName : "Sachin",
+  lastName :  "Tendulkar",
+}
+
+let printFullName = function (homeTown,pincode) {
+  console.log(this.firstName + " " + this.lastName + " from "+homeTown + " "+ pincode);
+}
+let fun = printFullName.bind(name1,"Bihar","844101")
+console.log("fun",fun)
+fun()
