b64566611521989afe86c991a2e1be29fff699c3
snehil <snehil@192.168.1.8>
2024-03-21 21:34:22 +0530

    updated myFilter

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 
 let arr = [ 1,2,3,4 ];
 
-Array.prototype.myMap = function(callback){
+Array.prototype.myFilter = function(callback){
    console.log("callback -> ",callback)
    let result = []
    for(let i = 0 ; i<this.length ; i++){
-     result.push(callback(this[i]))
+     this[i]>2 && result.push(callback(this[i]))
    }
    return result
 }
 
-console.log(arr.myMap((el)=>el*2))
+console.log(arr.myFilter((el)=>el))
