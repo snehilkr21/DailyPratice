8c81e6b3848b5a45635ebf9ed58f5428b6d4f219
snehil <snehil@192.168.1.8>
2024-03-21 22:27:31 +0530

    flat polyfill

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-let arr = [1,2,3,5]
-
-Array.prototype.myReduce = function(callbackFn,initialVal){
-    let res=initialVal
-    for(let i=0;i<this.length;i++){
-       res = !res ? this[i] : callbackFn(res,this[i])
-    }
-    return res;
+Array.prototype.myFlat = function (depth){
+    let res=[]
+    this.forEach((el)=>{
+         console.log(el)  
+        if(Array.isArray(el) && depth>0){
+            res.push(el.myFlat(...el,depth-1))
+        }else{
+            res.push(el)
+        }
+    })
+    return res
 }
+let arr = [1,2,[[3,4]],5,6]
+console.log("result "+arr.myFlat(1))
 
-console.log(arr.myReduce((acc,curr)=>{
-     return acc+curr
-},0))
