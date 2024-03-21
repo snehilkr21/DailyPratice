975f8f1b98242cec05ded02e7cb8338d4160420b
snehil <snehil@192.168.1.8>
2024-03-22 00:25:04 +0530

    polyfill for aplly

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-Array.prototype.myFlat = function (depth){
-    let res=[]
-    this.forEach((el)=>{
-         console.log(el)  
-        if(Array.isArray(el) && depth>0){
-            res.push(el.myFlat(...el,depth-1))
-        }else{
-            res.push(el)
-        }
-    })
-    return res
+let student = {
+    firstName : "Snehil",
+    lastName  : "Raj"
 }
-let arr = [1,2,[[3,4]],5,6]
-console.log("result "+arr.myFlat(1))
+
+function printName(state,age,country){
+    console.log("77 ",country,age)
+    console.log(this.firstName + " " + this.lastName + " state " + state + " age "+ age + " country " + country)
+}
+
+Function.prototype.myApply =  function (obj, ...args){
+    console.log("console ", obj,args[0],args)
+    if(typeof this !== 'function'){
+        return new Error("Not callabel")
+    }else{
+        obj.fn = this
+        obj.fn(...args[0])
+    }
+}
+printName.myApply(student, ["Bihar",30,"India"])
 
