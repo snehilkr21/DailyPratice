23f767bb825c521ef9794037b4328e78deecc3ad
snehil <snehil@192.168.1.4>
2024-05-27 23:36:33 +0530

    another attempt for promise

--- App.js
+++ App.js
@@ -1,45 +1,43 @@
 const promise1 = new Promise(function(resolve,reject){
-    setTimeout(()=>{
+    setTimeout(() => {
         resolve(33)
-    },100)
+    }, 100);
 })
 const promise2 = new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        reject(33)
-    },200)
-})
-const promise3 = new Promise(function(resolve,reject){
-    setTimeout(()=>{
+    setTimeout(() => {
         resolve(33)
-    },300)
+    }, 100);
 })
+const promise3 = Promise.reject(55);
 
-Promise.myAll = function(values){
-   const pr = new Promise(function(resolve,reject){
-        let result = [];
+Promise.myAll = function (values){
+    const pr = new Promise(function (resolve,reject){
+        let result = []
+        let completed = 0;
         let length =values.length
-        let complted =0
         values.forEach((item,index)=>{
             Promise.resolve(item)
             .then((res)=>{
-                result[index]=res;
-                complted++
-                if(complted==length){
+                completed++;
+                result[index]=res
+                if(completed==length){
                     resolve(result)
                 }
             })
             .catch((err)=>{
-                reject(new Error(`fails ${err}`))
+                reject(err)
             })
         })
-   })
-   return pr;
+    })
+
+
+    return pr;
 }
 
 Promise.myAll([promise1,promise2,promise3])
 .then((res)=>{
-    console.log("res",res);
+    console.log("res",res)
 })
 .catch((err)=>{
-    console.log("err",err.message)
+    console.log("err",err)
 })
