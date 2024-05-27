4151bfaf150c7cbdd45d4172415f4206b702a88a
snehil <snehil@192.168.1.4>
2024-05-28 01:03:27 +0530

    Promise.any

--- App.js
+++ App.js
@@ -1,35 +1,44 @@
 const Promise1 = new Promise(function(resolve,reject){
     setTimeout(()=>{
-       reject(33)
-    },500)
+        reject(33)
+    },100)
 })
 const Promise2 = new Promise(function(resolve,reject){
     setTimeout(()=>{
-        resolve(44)
-    },1000)
+        reject(55)
+    },200)
 })
 
-Promise.myRace = function(values){
+
+Promise.myAny = function(values){
     const pr = new Promise(function(resolve,reject){
+        let complete = 0;
+        let length = values.length
+        let err1 = []
+        function check(){
+            if(complete==length){
+                reject(new AggregateError(err1))
+            }
+        }
         values.forEach((item,index)=>{
             Promise.resolve(item)
             .then((res)=>{
                 resolve(res)
             })
             .catch((err)=>{
-                reject(err)
+                err1[index]=err
+                complete++;
+                check()
             })
         })
     })
-
-
-    return pr
+    return pr;
 }
 
-Promise.myRace([Promise1,Promise2])
+Promise.myAny([Promise1,Promise2])
 .then((res)=>{
     console.log("res",res)
 })
 .catch((err)=>{
-    console.log("err",err);
+    console.log("err",err.errors)
 })
