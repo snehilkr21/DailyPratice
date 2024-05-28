8a8f15b1f84a6e00a6c0e5e1bd0a97ae860802ff
snehil <snehil@192.168.1.5>
2024-05-28 20:32:07 +0530

    using normal promise and async/await

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
-const Promise1 = Promise.reject(12)
-const Promise2 = Promise.reject(13)
-const Promise3 = new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        reject(14)
-    },14)
-})
 
-Promise.myAny = function(values){
-    const pr = new Promise(function(resolve,reject){
-        let completed = 0;
-        let result = []
-        let length = values.length
-        function check(){
-            if(completed==length){
-                reject(new AggregateError(result))
-            }
-        }
-        values.forEach((item,index)=>{
-            Promise.resolve(item)
-            .then((res)=>{
-                resolve(res)
-            })
-            .catch((err)=>{
-                result[index] = err
-                completed++;
-                check()
-            })
-        })
-    }) 
-    return pr;
+async function getData(){
+  let data = await fetch("https://api.github.com/users/snehilkr21")
+  data = await data.json()
+  console.log("555")
+  console.table(data)
 }
+getData()
 
-Promise.myAny([Promise1,Promise2,Promise3])
-.then((res)=>{
-    console.log("res",res)
-})
-.catch((err)=>{
-    console.log("err",err.errors)
-})
+function getData1(){
+    fetch("https://api.github.com/users/snehilkr21")
+    .then((res)=>{
+        return res.json()
+    })
+    .then((res)=>{
+        console.log("1616")
+        console.table(res)
+    })
+    console.log("Namaste javascript")
+}
+getData1()
