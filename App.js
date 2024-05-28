80e87e375682e519770c249e1cb15a100c097f31
snehil <snehil@192.168.1.5>
2024-05-28 17:55:17 +0530

    Pratice Promise.allSettled

--- App.js
+++ App.js
@@ -1,43 +1,48 @@
-const Promise1 = new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        resolve(100)
-    },100)
-}) 
-const Promise2 = new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        resolve(200)
-    },200)
-})
+const Promise1 = Promise.resolve(100)
+const Promise2 = Promise.reject(200)
 const Promise3 = new Promise(function(resolve,reject){
     setTimeout(()=>{
-        reject(300)
+        resolve(300)
     },300)
 })
 
-Promise.myAll = function(values){
-   const pr = new Promise(function(resolve,reject){
-    let result = []
-    let length = values.length
-    let completed = 0;
-    values.forEach((item,index)=>{
-        Promise.resolve(item)
-        .then((res)=>{
-            result[index]=res;
-            completed++;
-            if(completed==length){
+
+
+Promise.mySettled = function(values){
+    const pr = new Promise(function(resolve,reject){
+         let result = [];
+         let length = values.length
+         let completed = 0
+         function check(){
+            if(length==completed){
                 resolve(result)
             }
-        })
-        .catch((err)=>{
-            reject(err)
-        })
+         }
+         values.forEach((item,index)=>{
+            Promise.resolve(item)
+            .then((res)=>{
+                result[index]={
+                    "status" : "fulfilled",
+                    "value" : res
+                }
+                completed++;
+                check()
+            })
+            .catch((err)=>{
+                result[index]={
+                    "status" : "rejected",
+                    "value" : err
+                }
+                completed++;
+                check() 
+            })
+         })
     })
-   })
-   return pr;
+    return pr;
 }
-Promise.myAll([Promise1,Promise2,Promise3])
+Promise.mySettled([Promise1,Promise2,Promise3])
 .then((res)=>{
-    console.log("res",res)
+    console.table(res)
 })
 .catch((err)=>{
     console.log("err",err)
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 4)Promise is a object which represent the eventual completion/failure of async operation and its result value.
 5)Promise object are immutable, we cannot modify the content of object.
 6)Promise.all takes the array of promises and return the value of promise result in an array. if any one promise fails then it immdetaily throw error.
-7)Promise.allSettled takes the array of promise and resturn the array of object(having status and value). it waits for all promise to resolve. 
+7)Promise.allSettled takes the array of promise and resturn the array of object(having status and value). it waits for all promise to resolve. It donot come inside the .catch
 8)Promise.race takes the array of promise and return the value of first settled(either resolve or reject). If first promise takes 500ssec to resolve and second takes 500sec to reject then it gives the result of reolve one.
 9)Promise.any takes the array of promise and return the first resolved . if all promise fails then we have to throw new AggregateError which is captured using err.errors
 10)
