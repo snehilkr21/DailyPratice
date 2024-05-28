86b63e1a327c9fdc4d58230cc59d30e6ed415aa4
snehil <snehil@192.168.1.5>
2024-05-28 18:12:19 +0530

    Promise.race

--- App.js
+++ App.js
@@ -1,48 +1,29 @@
-const Promise1 = Promise.resolve(100)
-const Promise2 = Promise.reject(200)
+const Promise1 = Promise.reject(12)
+const Promise2 = Promise.reject(13)
 const Promise3 = new Promise(function(resolve,reject){
     setTimeout(()=>{
-        resolve(300)
-    },300)
+        resolve(14)
+    },14)
 })
 
-
-
-Promise.mySettled = function(values){
+Promise.myRace =  function(values){
     const pr = new Promise(function(resolve,reject){
-         let result = [];
-         let length = values.length
-         let completed = 0
-         function check(){
-            if(length==completed){
-                resolve(result)
-            }
-         }
-         values.forEach((item,index)=>{
+        values.forEach((item,index)=>{
             Promise.resolve(item)
             .then((res)=>{
-                result[index]={
-                    "status" : "fulfilled",
-                    "value" : res
-                }
-                completed++;
-                check()
+                resolve(res)
             })
             .catch((err)=>{
-                result[index]={
-                    "status" : "rejected",
-                    "value" : err
-                }
-                completed++;
-                check() 
+                reject(err);
             })
-         })
+        })
     })
     return pr;
 }
-Promise.mySettled([Promise1,Promise2,Promise3])
+
+Promise.myRace([Promise1,Promise2,Promise3])
 .then((res)=>{
-    console.table(res)
+    console.log("res",res)
 })
 .catch((err)=>{
     console.log("err",err)
